c980e7f58b17dc22e8ee3099f9b378af08cce648
Camilo Gil <camilo.gil27071997@gmail.com>
2024-02-02 17:52:44 -0500

    Se crea en la implementacion del RepositorioListaReproduccion la funcion guardarLista reproduccion

--- src/main/java/com/quipux/pruebapractica/dominio/repositorio/RepositorioListaReproduccion.java
+++ src/main/java/com/quipux/pruebapractica/dominio/repositorio/RepositorioListaReproduccion.java
@@ -1,13 +1,10 @@
 package com.quipux.pruebapractica.dominio.repositorio;
 
-import com.quipux.pruebapractica.dominio.core.GuardarListaReproduccion;
 import com.quipux.pruebapractica.dominio.core.ListaReproduccion;
 import com.quipux.pruebapractica.dominio.dto.ListaReproduccionDto;
 import com.quipux.pruebapractica.dominio.dto.RespuestaListaReproduccionDto;
 
-import javax.swing.text.html.Option;
 import java.util.List;
-import java.util.Optional;
 
 public interface RepositorioListaReproduccion {
     List<RespuestaListaReproduccionDto> obtenerListasReproduccion();
--- src/main/java/com/quipux/pruebapractica/persistencia/repository/ImplRepositorioListaReproduccion.java
+++ src/main/java/com/quipux/pruebapractica/persistencia/repository/ImplRepositorioListaReproduccion.java
@@ -1,14 +1,27 @@
 package com.quipux.pruebapractica.persistencia.repository;
 
-import com.quipux.pruebapractica.dominio.core.GuardarListaReproduccion;
+import com.quipux.pruebapractica.dominio.core.ListaReproduccion;
+import com.quipux.pruebapractica.dominio.dto.ListaReproduccionDto;
 import com.quipux.pruebapractica.dominio.dto.RespuestaListaReproduccionDto;
 import com.quipux.pruebapractica.dominio.repositorio.RepositorioListaReproduccion;
+import com.quipux.pruebapractica.persistencia.crud.CrudListaReproduccion;
+import com.quipux.pruebapractica.persistencia.entity.ListaReproduccionEntity;
+import com.quipux.pruebapractica.persistencia.mapper.MapeadorListaReproduccion;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class ImplRepositorioListaReproduccion implements RepositorioListaReproduccion {
+
+    private final CrudListaReproduccion crudListaReproduccion;
+    private final MapeadorListaReproduccion mapeadorListaReproduccion;
+
+    public ImplRepositorioListaReproduccion(CrudListaReproduccion crudListaReproduccion, MapeadorListaReproduccion mapeadorListaReproduccion) {
+        this.crudListaReproduccion = crudListaReproduccion;
+        this.mapeadorListaReproduccion = mapeadorListaReproduccion;
+    }
+
     @Override
     public List<RespuestaListaReproduccionDto> obtenerListasReproduccion() {
         return null;
@@ -20,8 +33,9 @@ public class ImplRepositorioListaReproduccion implements RepositorioListaReprodu
     }
 
     @Override
-    public RespuestaListaReproduccionDto guardarListaReproduccion(GuardarListaReproduccion guardarListaReproduccion) {
-        return null;
+    public ListaReproduccionDto guardarListaReproduccion(ListaReproduccion listaReproduccion) {
+        ListaReproduccionEntity entity =mapeadorListaReproduccion.convertirAEntity(listaReproduccion);
+        return mapeadorListaReproduccion.convertirARespuesta(crudListaReproduccion.save(entity));
     }
 
     @Override
