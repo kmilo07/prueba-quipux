088c977e95b6917d455fef501582b678c756f9ac
Camilo Gil <camilo.gil27071997@gmail.com>
2024-02-02 17:54:11 -0500

    Se realiza en la ImplRepositorioCancion el metodo guardarCanciones

--- src/main/java/com/quipux/pruebapractica/persistencia/repository/ImplRepositorioCancion.java
+++ src/main/java/com/quipux/pruebapractica/persistencia/repository/ImplRepositorioCancion.java
@@ -3,14 +3,28 @@ package com.quipux.pruebapractica.persistencia.repository;
 import com.quipux.pruebapractica.dominio.core.Cancion;
 import com.quipux.pruebapractica.dominio.dto.CancionDto;
 import com.quipux.pruebapractica.dominio.repositorio.RepositorioCancion;
+import com.quipux.pruebapractica.persistencia.crud.CrudCancion;
+import com.quipux.pruebapractica.persistencia.entity.CancionEntity;
+import com.quipux.pruebapractica.persistencia.mapper.MapperCancion;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ImplRepositorioCancion implements RepositorioCancion {
+    private final CrudCancion crudCancion;
+    private final MapperCancion mapperCancion;
+
+    public ImplRepositorioCancion(CrudCancion crudCancion, MapperCancion mapperCancion) {
+        this.crudCancion = crudCancion;
+        this.mapperCancion = mapperCancion;
+    }
+
     @Override
     public List<CancionDto> obtenerCanciones() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
@@ -25,6 +39,6 @@ public class ImplRepositorioCancion implements RepositorioCancion {
 
     @Override
     public List<CancionDto> guardarCanciones(List<Cancion> canciones) {
-        return null;
+        return mapperCancion.convertiAListaCancionesDto((List<CancionEntity>) crudCancion.saveAll(mapperCancion.convertirAListaEntity(canciones)));
     }
 }
